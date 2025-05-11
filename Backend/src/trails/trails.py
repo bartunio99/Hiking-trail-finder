85313d2618479b9e36d0d5679ca638caec6a8f3f
bartunio99 <bart7112@gmail.com>
2025-05-11 04:16:16 +0100

    added trail validation

--- Backend/src/trails/trails.py
+++ Backend/src/trails/trails.py
@@ -2,9 +2,11 @@
 import overpy
 from pymongo import MongoClient
 from geopy.geocoders import Nominatim
+from motor.motor_asyncio import AsyncIOMotorClient
 
 DB_NAME = 'trail_db'
 HOST = "mongodb://localhost:27017/"
+
 
 def get_place_coordinates(place_name):
     geolocator = Nominatim(user_agent="trail_locator")
@@ -23,11 +25,6 @@ def get_trails_nearby(lat, lon, radius_km=5):
     radius_deg = radius_km / 111.0
     radius_m = radius_km * 1000.0
 
-    min_lat = lat - radius_deg
-    max_lat = lat + radius_deg
-    min_lon = lon - radius_deg
-    max_lon = lon + radius_deg
-
     query = f"""
         [out:json][timeout:60];
         (
@@ -44,24 +41,23 @@ def get_trails_nearby(lat, lon, radius_km=5):
         out skel qt;
         """
 
-
     try:
         result = api.query(query)
         trails = []
 
-        #valid_ways = [way for way in result.ways if len(way.nodes) > 3]
-
-
+        
         for way in result.ways:
             trail = {
                 "name": way.tags.get("name", "Unnamed Trail"),
-                "sac_scale": way.tags.get("sac_scale", "N/A"),
+                "sac_scale": way.tags.get("sac_scale"),
                 "tags": way.tags,
                 "nodes": [(node.lat, node.lon) for node in way.nodes],
             }
             trails.append(trail)
 
-        return trails
+        valid_trails = [trail for trail in trails if trail.get("sac_scale") is not None]
+
+        return valid_trails
 
     except Exception as e:
         print(f"Error: {e}")
@@ -71,9 +67,8 @@ def store_trails_in_mongodb(trails, name):
     # Connect to MongoDB (make sure MongoDB is running)
     client = MongoClient(HOST)
     db = client[DB_NAME]  # Use or create database 'trail_db'
-    collection_name = 'trails_' + name
-    collection = db[collection_name]  # Use or create collection 'trails'
 
+    collection = db[name]  # Use or create collection with the name of the place
     # Insert trail data into MongoDB
     if trails:
         #clean decimal to float type (coordinates)
@@ -92,8 +87,10 @@ def load_local_trails(location_name:str, radius:float):
         trails = get_trails_nearby(latitude,longitude,radius)
 
         store_trails_in_mongodb(trails, location_name)
+        return True
     except ValueError as e:
-        print(e)
+        return False
+        print(f"Error: {e}")
         exit(1)
 
 
